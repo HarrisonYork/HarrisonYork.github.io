d777e8527039df31ed3476ced18ee0a2ff6b329e
Harrison York <harrison.york@duke.edu>
2022-11-14 14:42:43 -0500

    commit hex

--- src/index.js
+++ src/index.js
@@ -15,27 +15,51 @@ class App extends Component {
 
     this.data = [
       {
-        id: 70000,
+        id: 458752,
         start: new Date(2022, 11-1, 6, 18, 16, 17),
-        end: new Date(2022, 11, 7, 13, 58, 34),
+        end: new Date(2022, 11-1, 7, 13, 58, 34),
         name: "Tool Tag 1"
-      },
+        },
         {
-        id: 70000,
+        id: 454656,
+        start: new Date(2022, 11-1, 7, 13, 59, 3),
+        end: new Date(2022, 11-1, 7, 14, 2, 52),
+        name: "Tool Tag 2"
+        },
+        {
+        id: 458752,
         start: new Date(2022, 11-1, 7, 13, 59, 16),
-        end: new Date(2022, 11, 7, 14, 3, 8),
+        end: new Date(2022, 11-1, 7, 14, 3, 8),
         name: "Tool Tag 1"
         },
         {
-        id: 70000,
+        id: 454656,
+        start: new Date(2022, 11-1, 7, 14, 6, 19),
+        end: new Date(2022, 11-1, 7, 14, 15, 2),
+        name: "Tool Tag 2"
+        },
+        {
+        id: 458752,
         start: new Date(2022, 11-1, 7, 14, 6, 36),
-        end: new Date(2022, 11, 7, 14, 15, 5),
+        end: new Date(2022, 11-1, 7, 14, 15, 5),
         name: "Tool Tag 1"
         },
         {
-        id: 70000,
+        id: 503808,
+        start: new Date(2022, 11-1, 7, 14, 14, 24),
+        end: new Date(2022, 11-1, 7, 14, 14, 44),
+        name: "Tool Tag 3"
+        },
+        {
+        id: 503808,
+        start: new Date(2022, 11-1, 7, 14, 15, 23),
+        end: new Date(2022, 11-1, 7, 14, 15, 35),
+        name: "Tool Tag 3"
+        },
+        {
+        id: 458752,
         start: new Date(2022, 11-1, 7, 14, 15, 29),
-        end: new Date(2022, 11, 14, 14, 13, 1),
+        end: new Date(2022, 11-1, 14, 14, 13, 1),
         name: "Tool Tag 1"
         }
     ];
